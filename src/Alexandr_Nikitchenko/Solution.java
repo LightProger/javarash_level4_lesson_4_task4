a50928a63bb37f98c4409b0b43168b02132ae552
Alexandr <photoluxe@mail.ru>
2017-01-16 22:20:42 +0600

    Initial commit

---
+++ src/Alexandr_Nikitchenko/Solution.java
@@ -0,0 +1,38 @@
+package Alexandr_Nikitchenko;
+
+
+/* Время года
+Реализовать метод checkSeason. По номеру месяца, метод должен определить время года (зима, весна, лето, осень) и вывести на экран.
+Пример для номера месяца 2:
+зима
+Пример для номера месяца 5:
+весна
+*/
+
+public class Solution
+{
+    public static void main(String[] args) {
+        checkSeason(12);
+        checkSeason(4);
+        checkSeason(7);
+        checkSeason(10);
+    }
+
+    public static void checkSeason(int mount){
+        if(mount == 12){
+            System.out.println("Зима");
+        } else
+        if(mount >= 1 && mount <= 2 ){
+            System.out.println("Зима");
+        }else
+        if(mount >= 3 && mount <= 5){
+            System.out.println("Весна");
+        }else
+        if (mount >= 6 && mount <= 8){
+            System.out.println("Лето");
+        }else
+        if (mount >= 9 && mount <= 11) {
+            System.out.println("Осень");
+        }
+    }
+}
